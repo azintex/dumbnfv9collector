bfe8d1b20f67e3535232c26940688b7b71df0d54
unknown <h.uzdenov@azintex.com>
2019-11-24 03:50:31 +0400

    test

--- testcollector.py
+++ testcollector.py
@@ -8,18 +8,12 @@ sock.bind(('172.16.93.5', 2055))
 if __name__ == "__main__":
     while True:
         data = sock.recv(4096)
-        # Flow header
-        #fh = unpack('!HHLLLL', data[0:20])
-        # FlowSet ID
-        fsId = unpack('!H', data[20:22])
-        if fsId[0] == 0:
-            tfs = unpack_from('!HHHHH', data, 22)
-            print(str(tfs))
-        else:        
-            fs = unpack_from('!LLBHHLLLL', data, 24)
-            print(json.dumps(fs))
-        # Template FlowSet
-        #tfs = unpack('!HH', data[20:24])
-        # FlowSet
-        #fs = unpack('!HHHH', data[20:28])
-        #print(str(fs))
+        fsHeader = unpack('!HHLLLL', data[:20])
+        print(json.dumps(fsHeader))
+        #fsId = unpack('!H', data[20:22])
+        #if fsId[0] == 0:
+            #tfs = unpack_from('!HHHHH', data, 22)
+            #print(str(tfs))
+        #else:        
+            #fs = unpack_from('!LLBHHLLLL', data, 24)
+            #print(json.dumps(fs))
