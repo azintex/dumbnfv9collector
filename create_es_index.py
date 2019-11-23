ce627bdc838a2bce3443a348a68166ba5916cb8c
unknown <h.uzdenov@azintex.com>
2019-11-23 23:46:52 +0400

    move create index to `create_es_index`

---
+++ create_es_index.py
@@ -0,0 +1,82 @@
+from es import createIndex, _es
+from datetime import date
+
+# Default index name prefix is `netflow-v9-`
+indexPrefix = 'netflow-v9-'
+indexName = indexPrefix + date.today()
+
+settings = {
+    "settings" : {
+        "number_of_shards" : 1,
+        "number_of_replicas": 0
+    },
+    "mappings": {
+        "properties": {
+            "sysUptimeFirst": {
+                "type": "text"
+            },
+            "sysUptimeLast": {
+                "type": "text"
+            },
+            "counterBytes": {
+                "type": "text"
+            },
+            "counterPackets": {
+                "type": "text"
+            },
+            "inputInterface": {
+                "type": "text"
+            },
+            "outputInterface": {
+                "type": "text"
+            },
+            "ipv4SrcAddr": {
+                "type": "text"
+            },
+            "ipv4DstAddr": {
+                "type": "text"
+            },
+            "ipProtocol": {
+                "type": "text"
+            },
+            "ipTos": {
+                "type": "text"
+            },
+            "transportSrcPort": {
+                "type": "text"
+            },
+            "transportDstPort": {
+                "type": "text"
+            },
+            "flowSampler": {
+                "type": "text"
+            },
+            "ipv4NextHop": {
+                "type": "text"
+            },
+            "ipv4DstMask": {
+                "type": "text"
+            },
+            "ipv4SrcMask": {
+                "type": "text"
+            },
+            "tcpFlags": {
+                "type": "text"
+            },
+            "destinationAS": {
+                "type": "text"
+            },
+            "sourceAS": {
+                "type": "text"
+            }
+        }
+    }
+}
+
+try:
+    createIndex(_es, index=indexName, body=settings)
+    print('Index created')
+except Exception as ex:
+    print(str(ex))
+finally:
+    print('Index not created')
--- nfcollector.py
+++ nfcollector.py
@@ -4,98 +4,19 @@ from time import time
 import config as cfg
 from es import createIndex, addFlow, _es
 
-
-
-
-
-#runMode = cfg.mode
-#ipAddress = cfg.ip_address
-#port = cfg.port
 templSize = cfg.template_size_in_bytes
 
-if cfg.caption_duration != 0:
+if cfg.capture_duration != 0:
     captDur = time() + cfg.caption_duration
-else:
-    captDur = time() - 100
-# Init socket
+
+# Socket initialization
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# Binding
+
+# Socket binding
 sock.bind((cfg.ip_address, cfg.port))
 
 # Date stamps
 td = str(date.today())
-
-setBody = {
-    "settings" : {
-        "number_of_shards" : 1,
-        "number_of_replicas": 0
-    },
-    "mappings": {
-        "properties": {
-            "sysUptimeFirst": {
-                "type": "text"
-            },
-            "sysUptimeLast": {
-                "type": "text"
-            },
-            "counterBytes": {
-                "type": "text"
-            },
-            "counterPackets": {
-                "type": "text"
-            },
-            "inputInterface": {
-                "type": "text"
-            },
-            "outputInterface": {
-                "type": "text"
-            },
-            "ipv4SrcAddr": {
-                "type": "text"
-            },
-            "ipv4DstAddr": {
-                "type": "text"
-            },
-            "ipProtocol": {
-                "type": "text"
-            },
-            "ipTos": {
-                "type": "text"
-            },
-            "transportSrcPort": {
-                "type": "text"
-            },
-            "transportDstPort": {
-                "type": "text"
-            },
-            "flowSampler": {
-                "type": "text"
-            },
-            "ipv4NextHop": {
-                "type": "text"
-            },
-            "ipv4DstMask": {
-                "type": "text"
-            },
-            "ipv4SrcMask": {
-                "type": "text"
-            },
-            "tcpFlags": {
-                "type": "text"
-            },
-            "destinationAS": {
-                "type": "text"
-            },
-            "sourceAS": {
-                "type": "text"
-            }
-        }
-    }
-}
-
-
-# if __name__ == "__main__":
-#     createIndex(_es, "netflow-v9", setBody)
 
 def startCapture(mode):
     if not os.path.exists('dumps') and mode == 'raw':
@@ -114,30 +35,52 @@ def startCapture(mode):
     while mode == 'raw' and time() < captDur:
         data = sock.recv(1518)
         print(data)
+    
+    # captDur set capture duration in seconds, in case is not exist capture will be infinity
 
-    while mode == 'unpack' and time() < captDur:
-        data = sock.recv(1518)
-        nfHeader = struct.unpack('!HHLLLL', data[0:20])
-        #print(nfHeader)
+    try:
+        if not captDur:
+            while True:
+                data = sock.recv(1522)
+                flowHeader = struct.unpack('!HHLLLL', data[0:20])
 
-        for flow in range(0, nfHeader[1]):
-            if flow == 0:
-                firstFlow = struct.unpack('!IIIIIIIIBBHHBIBBBHH', data[24:74])
-                #print(firstFlow)
-                addFlow(_es, 'netflow-v9', {"sysUptimeFirst": firstFlow[0], "sysUptimeLast": firstFlow[1], "counterBytes": firstFlow[2], \
-                    "counterPackets": firstFlow[3], "inputInterface": firstFlow[4], "outputInterface": firstFlow[5], "ipv4SrcAddr": firstFlow[6], \
-                        "ipv4DstAddr": firstFlow[7], "ipProtocol": firstFlow[8], "ipTos": firstFlow[9], "transportSrcPort": firstFlow[10], \
-                            "transportDstPort": firstFlow[11], "flowSampler": firstFlow[12], "ipv4NextHop": firstFlow[13], "ipv4DstMask": firstFlow[14], \
-                                "ipv4SrcMask": firstFlow[15], "tcpFlags": firstFlow[16], "destinationAS": firstFlow[17], "sourceAS": firstFlow[18]})
-            else:
-                offset = flow * templSize
-                subseqFlow = struct.unpack('!IIIIIIIIBBHHBIBBBHH', data[24 + offset:74 + offset])
-                addFlow(_es, 'netflow-v9', {"sysUptimeFirst": subseqFlow[0], "sysUptimeLast": subseqFlow[1], "counterBytes": subseqFlow[2], \
-                    "counterPackets": subseqFlow[3], "inputInterface": subseqFlow[4], "outputInterface": subseqFlow[5], "ipv4SrcAddr": subseqFlow[6], \
-                        "ipv4DstAddr": subseqFlow[7], "ipProtocol": subseqFlow[8], "ipTos": subseqFlow[9], "transportSrcPort": subseqFlow[10], \
-                            "transportDstPort": subseqFlow[11], "flowSampler": subseqFlow[12], "ipv4NextHop": subseqFlow[13], "ipv4DstMask": subseqFlow[14], \
-                                "ipv4SrcMask": subseqFlow[15], "tcpFlags": subseqFlow[16], "destinationAS": subseqFlow[17], "sourceAS": subseqFlow[18]})
-                #print(subseqFlow)
+                for flow in range(0, flowHeader[1]):
+                    if flow == 0:
+                        firstFlow = struct.unpack('!IIIIIIIIBBHHBIBBBHH', data[24:74])
+                        addFlow(_es, 'netflow-v9', {"sysUptimeFirst": firstFlow[0], "sysUptimeLast": firstFlow[1], "counterBytes": firstFlow[2], \
+                            "counterPackets": firstFlow[3], "inputInterface": firstFlow[4], "outputInterface": firstFlow[5], "ipv4SrcAddr": firstFlow[6], \
+                                "ipv4DstAddr": firstFlow[7], "ipProtocol": firstFlow[8], "ipTos": firstFlow[9], "transportSrcPort": firstFlow[10], \
+                                    "transportDstPort": firstFlow[11], "flowSampler": firstFlow[12], "ipv4NextHop": firstFlow[13], "ipv4DstMask": firstFlow[14], \
+                                        "ipv4SrcMask": firstFlow[15], "tcpFlags": firstFlow[16], "destinationAS": firstFlow[17], "sourceAS": firstFlow[18]})
+                    else:
+                        offset = flow * templSize
+                        subseqFlow = struct.unpack('!IIIIIIIIBBHHBIBBBHH', data[24 + offset:74 + offset])
+                        addFlow(_es, 'netflow-v9', {"sysUptimeFirst": subseqFlow[0], "sysUptimeLast": subseqFlow[1], "counterBytes": subseqFlow[2], \
+                            "counterPackets": subseqFlow[3], "inputInterface": subseqFlow[4], "outputInterface": subseqFlow[5], "ipv4SrcAddr": subseqFlow[6], \
+                                "ipv4DstAddr": subseqFlow[7], "ipProtocol": subseqFlow[8], "ipTos": subseqFlow[9], "transportSrcPort": subseqFlow[10], \
+                                    "transportDstPort": subseqFlow[11], "flowSampler": subseqFlow[12], "ipv4NextHop": subseqFlow[13], "ipv4DstMask": subseqFlow[14], \
+                                        "ipv4SrcMask": subseqFlow[15], "tcpFlags": subseqFlow[16], "destinationAS": subseqFlow[17], "sourceAS": subseqFlow[18]})
+    finally:
+        while time() < cfg.capture_duration:
+            data = sock.recv(1522)
+            flowHeader = struct.unpack('!HHLLLL', data[0:20])
+
+            for flow in range(0, flowHeader[1]):
+                if flow == 0:
+                    firstFlow = struct.unpack('!IIIIIIIIBBHHBIBBBHH', data[24:74])
+                    addFlow(_es, 'netflow-v9', {"sysUptimeFirst": firstFlow[0], "sysUptimeLast": firstFlow[1], "counterBytes": firstFlow[2], \
+                        "counterPackets": firstFlow[3], "inputInterface": firstFlow[4], "outputInterface": firstFlow[5], "ipv4SrcAddr": firstFlow[6], \
+                            "ipv4DstAddr": firstFlow[7], "ipProtocol": firstFlow[8], "ipTos": firstFlow[9], "transportSrcPort": firstFlow[10], \
+                                "transportDstPort": firstFlow[11], "flowSampler": firstFlow[12], "ipv4NextHop": firstFlow[13], "ipv4DstMask": firstFlow[14], \
+                                    "ipv4SrcMask": firstFlow[15], "tcpFlags": firstFlow[16], "destinationAS": firstFlow[17], "sourceAS": firstFlow[18]})
+                else:
+                    offset = flow * templSize
+                    subseqFlow = struct.unpack('!IIIIIIIIBBHHBIBBBHH', data[24 + offset:74 + offset])
+                    addFlow(_es, 'netflow-v9', {"sysUptimeFirst": subseqFlow[0], "sysUptimeLast": subseqFlow[1], "counterBytes": subseqFlow[2], \
+                        "counterPackets": subseqFlow[3], "inputInterface": subseqFlow[4], "outputInterface": subseqFlow[5], "ipv4SrcAddr": subseqFlow[6], \
+                            "ipv4DstAddr": subseqFlow[7], "ipProtocol": subseqFlow[8], "ipTos": subseqFlow[9], "transportSrcPort": subseqFlow[10], \
+                                "transportDstPort": subseqFlow[11], "flowSampler": subseqFlow[12], "ipv4NextHop": subseqFlow[13], "ipv4DstMask": subseqFlow[14], \
+                                    "ipv4SrcMask": subseqFlow[15], "tcpFlags": subseqFlow[16], "destinationAS": subseqFlow[17], "sourceAS": subseqFlow[18]})
 
 if __name__ == '__main__':
     startCapture(cfg.mode)
