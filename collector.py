d66b655ade2cee9ee2fa4522d33e72ffcd105549
unknown <h.uzdenov@azintex.com>
2019-11-26 00:57:05 +0400

    work test

--- collector.py
+++ collector.py
@@ -16,22 +16,21 @@ index = es['index']['prefix'] + es['index']['name']
 
 
 def getDataRecordFlowSets():
-    #while True:
-        data = sock.recv(4096)
-        # First unpack from bytes FlowSet header, FlowSet ID and FlowSet length.
-        fsHFL = unpack_from('!HHLLLLHH', data)
-        # `addFlow` function insert NetFlow v9 header data in Elasticsearch.
-        addFlow(index, {"version": fsHFL[0], "count": fsHFL[1], \
+    data = sock.recv(4096)
+    # First unpack from bytes FlowSet header, FlowSet ID and FlowSet length.
+    fsHFL = unpack_from('!HHLLLLHH', data)
+    # `addFlow` function insert NetFlow v9 header data in Elasticsearch.
+    addFlow(index, {"version": fsHFL[0], "count": fsHFL[1], \
                         "sysUptime": fsHFL[2], "unixSeconds": fsHFL[3], \
                             "packageSequence": fsHFL[4], "sourceId": fsHFL[5]})
-        # Check for FlowSet ID. Data record FlowSet ID greater than 255. 
-        # Template record FlowSet ID in 0-255 range.
-        if fsHFL[6] != 0:
-            # First get unpacked iterable. 
-            # The buffer’s size in bytes must be a multiple of the size required by the format (c)
-            fs = iter_unpack('!LLBHHLLLL', data[24:fsHFL[1] * clr['ts'] + 24])
-            for flow in fs:
-                addFlow(index, {"ipv4SourceAddress": flow[0], \
+    # Check for FlowSet ID. Data record FlowSet ID greater than 255. 
+    # Template record FlowSet ID in 0-255 range.
+    if fsHFL[6] != 0:
+        # First get unpacked iterable. 
+        # The buffer’s size in bytes must be a multiple of the size required by the format (c)
+        fs = iter_unpack('!LLBHHLLLL', data[24:fsHFL[1] * clr['ts'] + 24])
+        for flow in fs:
+            addFlow(index, {"ipv4SourceAddress": flow[0], \
                                 "ipv4DestinationAddress": flow[1], \
                                     "ipProtocol": flow[2], \
                                         "transportSourcePort": flow[3], \
