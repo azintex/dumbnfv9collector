fc74c17c8b491cf87874ba47ae429f5c192d8c9d
azintex <h.uzdenov@azintex.com>
2022-12-17 01:25:29 +0400

    Update config.py
    Minor changes for variable names and doc.

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
-# Set collector mode `raw` or `unpack`
+# Collector mode will be 'raw' or 'unpack'.
+# 'unpack' mode is default.
 mode = 'unpack'
 
-# LIsten IP address.
+# Listen IP address.
 ip_address = '172.16.93.5'
 
 # Listen port (UDP).
 port = 2055
 
-# Template size in bytes. Template size configured on exporter.
-template_size_in_bytes = 50
+# Template size in bytes. Must be same size as template on exporter.
+template_size = 50
 
-# Capture duration in seconds
-caption_duration = 300
+# Capturing duration in seconds
+capturing_duration = 300
